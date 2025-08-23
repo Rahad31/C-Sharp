e89983c7d8e3c3d3f687a6af01ca242ca96338e2
Md Rahad <rahad3100@gmail.com>
2025-08-23 12:04:17 +0600

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp
+{
+    internal class Car
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public int Mileage { get; set; }
+
+        public void Drive(int kilometers)
+        {
+            Mileage += kilometers;
+            Console.WriteLine(
+                $"Car {Brand} {Model} has driven {kilometers} km. Total mileage: {Mileage} km.");
+        }
+
+        public void ShowCarInfo()
+        {
+            Console.WriteLine(
+                $"Car Info: Brand - {Brand}, Model - {Model}, Year - {Year}, Mileage - {Mileage}");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace C_Sharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car();
+                car1.Brand = "Toyota";
+                car1.Model = "Corolla";
+                car1.Year = 2020;
+                car1.Mileage = 15000;
+                car1.Drive(100);
+                car1.ShowCarInfo();
+
+            Car car2 = new Car();
+                car2.Brand = "Tesla";
+                car2.Model = "Sedan";
+                car2.Year = 2025;
+                car2.Mileage = 5000;
+                car2.Drive(5000);
+                car2.ShowCarInfo();
+
+
+
+            //Console.WriteLine("\n--- Car Information ---");
+
+            //Console.WriteLine("\nPress any key to exit...");
+            //Console.ReadKey();
+        }
+    }
+}
